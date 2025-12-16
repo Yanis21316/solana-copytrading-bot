74ac0ce03491a027428c45c9e5cb5629e88c6a6e
Yanis21316 <151951044+Yanis21316@users.noreply.github.com>
2025-12-16 19:10:45 +0100

    Update wssListener.ts

--- src/core/listener/wssListener.ts
+++ src/core/listener/wssListener.ts
@@ -1,17 +1,13 @@
 import { Connection, PublicKey } from "@solana/web3.js";
-import { COMMITMENT } from "../../config/constants.js";
-import { handleTransaction } from "../detector/tradeDetector.js";
-
-const connection = new Connection(
-  process.env.QUICKNODE_WSS!,
-  { commitment: COMMITMENT }
-);
 
 export function listenWallet(wallet: string) {
+  const connection = new Connection(process.env.QUICKNODE_WSS!, { commitment: "processed" });
   const pubkey = new PublicKey(wallet);
 
   connection.onLogs(pubkey, (logs) => {
     if (!logs.signature) return;
-    handleTransaction(logs.signature);
+    console.log("Transaction détectée:", logs.signature);
   });
+
+  console.log("➕ Wallet surveillé :", wallet);
 }
