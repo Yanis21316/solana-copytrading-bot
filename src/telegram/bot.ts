d4efb1997c032286b5116fd553674b19c4b2f434
Yanis21316 <151951044+Yanis21316@users.noreply.github.com>
2025-12-16 19:10:19 +0100

    Update bot.ts

--- src/telegram/bot.ts
+++ src/telegram/bot.ts
@@ -1,4 +1,13 @@
 import TelegramBot from "node-telegram-bot-api";
-import { ENV } from "../config/env.js";
 
-export const bot = new TelegramBot(ENV.TG_TOKEN, { polling: true });
+export const bot = new TelegramBot(process.env.TG_TOKEN!, { polling: true });
+
+// Commande /start
+bot.onText(/\/start/, (msg) => {
+  bot.sendMessage(msg.chat.id, "👋 Salut ! Bot actif !");
+});
+
+// Commande /status
+bot.onText(/\/status/, (msg) => {
+  bot.sendMessage(msg.chat.id, "✅ Bot actif, mode TEST");
+});
